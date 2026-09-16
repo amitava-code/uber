117c14730a13036bd39cb7fc8dce9c8a023aad15
Delta Student <217562233+amitava-code@users.noreply.github.com>
2026-09-16 20:39:57 +0530

    feat: add mongodb connection and dns configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
 const http = require('http')
 const app = require('./src/app')
 const port = process.env.PORT || 3000
+const connectDB = require("./src/db/db")
+
+const dns = require("dns")
+dns.setServers(["8.8.8.8", "1.1.1.1"])
+
+connectDB()
+
+
+
+
 
 
 
---
+++ backend/src/db/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+function connectDB(){
+    
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+        console.log("DB connected")
+    })
+    .catch((err)=>{
+        console.log("DB connection FAILED")
+        console.log(err)
+     
+    })
+}
+
+module.exports = connectDB
